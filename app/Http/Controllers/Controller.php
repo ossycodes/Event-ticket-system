64bbb282e274ec6471bbefc9596f8ef7d2c69c43
osaigbovoemmanuel1 <osaigbovoemmanuel1@gmail.com>
2018-10-19 14:39:29 +0100

    minor change

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,10 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use App\JSONResponse\JSONResponse\JSONResponse;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\JSONResponse;
 
 class Controller extends BaseController
 {

e094626d97ac326af8ab5d361a43168a006ef217
osaigbovoemmanuel1 <osaigbovoemmanuel1@gmail.com>
2018-11-27 15:14:49 +0100

    minro fixes

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use App\JSONResponse\JSONResponse\JSONResponse;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
@@ -11,5 +10,5 @@ use App\Helper\checkAndUploadImage;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, JSONResponse, ValidatesRequests, checkAndUploadImage;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, checkAndUploadImage;
 }
